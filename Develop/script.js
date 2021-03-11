c0e1efd8d7d00d7977b33891c09c0244d00c3add
ItsEvdok <78257029+ItsEvdok@users.noreply.github.com>
2021-03-10 22:02:48 -0500

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,16 @@
 // Assignment code here
-var passLength = window.prompt("How long would you like your password to be?")
 
 function generatePassword(){
-  //var passLength = window.prompt("How long would you like your password to be?");
-
-
+  var passLength = parseInt(prompt("How long would you like your password to be?"));
   var special = window.confirm("Would you like your password to include special characters?");
-
-
   var numbers = window.confirm("Would you like your password to include numbers?");
-
-
   var upper = window.confirm("Would you like your password to include uppercase letters?");
-
-
   var lower = window.confirm("Woudl you like your password to include lowercase letter?");
+
+  if(special === true){
+    return "!@#$%&*<>?";
+  }
+  
 
 };
 
